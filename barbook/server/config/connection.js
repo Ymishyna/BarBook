60d3e9b8bc695e6fa4422dcf084c1d2154f1766c
AlexandrZhydyk <zhydykalex@ukr.net>
2024-03-22 23:47:35 +0200

    for test

--- barbook/server/config/connection.js
+++ barbook/server/config/connection.js
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/barbook');
+mongoose.connect(
+  "mongodb+srv://yana92:Password@cluster0.kljl2og.mongodb.net/barbook?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/barbook');
 
 module.exports = mongoose.connection;
--- barbook/server/models/User.js
+++ barbook/server/models/User.js
@@ -1,10 +1,10 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // import schema from Book.js
-const bookSchema = require('./Book');
+const bookSchema = require("./Book");
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -15,7 +15,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      match: [/.+@.+\..+/, "Must use a valid email address"],
     },
     password: {
       type: String,
@@ -33,8 +33,8 @@ const userSchema = new Schema(
 );
 
 // hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -48,10 +48,10 @@ userSchema.methods.isCorrectPassword = async function (password) {
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
+userSchema.virtual("bookCount").get(function () {
   return this.savedBooks.length;
 });
 
-const User = model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
